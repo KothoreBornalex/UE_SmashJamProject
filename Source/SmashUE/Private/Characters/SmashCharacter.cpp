0f2ce8f96447bd4f9c93f042d12a6acfd24da87a
Ibrahim-Mallouka <panda.bornalex@gmail.com>
2024-09-17 17:55:14 +0200

    [Add] Orientation on spawn characters

--- Source/SmashUE/Private/Characters/SmashCharacter.cpp
+++ Source/SmashUE/Private/Characters/SmashCharacter.cpp
@@ -22,11 +22,32 @@ void ASmashCharacter::BeginPlay()
 void ASmashCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	RotateMeshUsingOrientX();
 }
 
 // Called to bind functionality to input
 void ASmashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+float ASmashCharacter::GetOrientX() const
+{
+	return OrientX;
+}
+
+void ASmashCharacter::SetOrientX(float NewOrientX)
+{
+	OrientX = NewOrientX;
+}
+
+void ASmashCharacter::RotateMeshUsingOrientX() const
+{
+	FRotator Rotation = GetMesh()->GetRelativeRotation();
+
+	Rotation.Yaw = -90.0f * OrientX;
+
+	GetMesh()->SetRelativeRotation(Rotation);
 }
 
